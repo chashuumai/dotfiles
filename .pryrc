9b35fa94ed15dcf90eb28dcba4b21398506ac6ee
penxera <kool.arakaze@gmail.com>
2014-04-05 20:57:07 +0900

    delete not use files

--- .pryrc
+++
@@ -1,4 +0,0 @@
-require 'rubygems' if RUBY_VERSION < '1.9'
-require 'awesome_print'
-
-Pry.print = proc{|output,value| output.puts value.ai }
--- init.sh
+++ init.sh
@@ -4,10 +4,6 @@
 #    ln -s $HOME/dotfiles/$file $HOME/$file
 #done
 
-ln -sf $HOME/dotfiles/.bashrc $HOME/
-ln -sf $HOME/dotfiles/.bash_profile $HOME/
-#ln -sf $HOME/dotfiles/.emacs.d $HOME/
 ln -sf $HOME/dotfiles/git/.gitignore $HOME/
 ln -sf $HOME/dotfiles/git/.gitconfig $HOME/
-#ln -sf $HOME/dotfiles/.pryrc $HOME/
 ln -sf $HOME/dotfiles/.zshrc $HOME/
